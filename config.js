ca5b4ba935fd4a540d79ad9d4e91ad6318d49bae
BGCMUBARAK <153381240+BGCMUBARAK@users.noreply.github.com>
2023-12-18 13:15:27 +0100

    Update config.js554

--- config.js
+++ config.js
@@ -74,12 +74,12 @@ module.exports = {
   ownernumber: process.env.Owner_Number || "2349150212473",
   instagramId: process.env.Insta || "8.08_only_mine",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
+  mongoUrl: process.env.MongoDB || "mongodb+srv://DARKSHAN:3000@cluster0.t1wsjlv.mongodb.net/",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "VEphNTJ_Queen-Anya_hZXo=", 
+  sessionId: process.env.SESSION_ID || "MGtGdzh_Queen-Anya_OZzQ=", 
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
@@ -110,7 +110,7 @@ global.author = process.env.Author || "@PBishara"
 global.packname = process.env.PackName || "Queen Anya v2 MD" 
 global.myweb = process.env.Web || "https://github.com/PikaBotz" 
 global.footer = process.env.Footer || "© Prime Bishara" 
-global.prefa = process.env.Prefix || [','] 
+global.prefa = process.env.Prefix || ['!'] 
 global.themeemoji = process.env.ThemeEmoji || "🎐" 
 global.ownername = process.env.Owner_Name || "Prime~Bishara" 
 global.ownernumber = process.env.Owner_Number || "2349150212473" 
